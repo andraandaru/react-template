17f372efa2c0546b2fa95bd37794318dcb0026f1
Andra Andaru <andraandaruu@gmail.com>
2022-01-17 19:40:44 +0700

    add svg loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import "../public/styles.css"
 import IMAGE from "./assets/images/react.png"
+import LOGO from "./assets/icon/logo.svg"
 
 const App = () => {
   return (
     <div>
       <h1>Hello World</h1>
       <img src={IMAGE} alt="React Logo" width={500} height={250} />
+      <img src={LOGO} alt="React Logo" width={300} height={300} />
     </div>
   )
 }
