6725f0afd5c39a79a3938ca6982a10dde9b112a4
Andra Andaru <andraandaruu@gmail.com>
2022-01-17 19:38:42 +0700

    add image loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import "../public/styles.css"
+import IMAGE from "./assets/images/react.png"
 
 const App = () => {
   return (
     <div>
       <h1>Hello World</h1>
+      <img src={IMAGE} alt="React Logo" width={500} height={250} />
     </div>
   )
 }
