568af7017b2fdd1c033abd4559020bd9684e6ad6
Andra Andaru <andraandaruu@gmail.com>
2022-01-18 14:26:23 +0700

    Setup from Scratch (3/8) setup webpack for multiple env

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,9 @@ import LOGO from "./assets/icon/logo.svg"
 const App = () => {
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>
+        Hello World from {process.env.NODE_ENV} {process.env.name}
+      </h1>
       <img src={IMAGE} alt="React Logo" width={500} height={250} />
       <img src={LOGO} alt="React Logo" width={300} height={300} />
     </div>
